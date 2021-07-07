2d5c866e9997926dc433828e3aa4bc0cb143f715
Alex <tanc.alexandru5@gmail.com>
2021-07-07 19:33:04 +0300

    Model + Routes

--- config/default.js
+++ config/default.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://ADMIN:parola321@pprojects.ex2kh.mongodb.net/urls?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://ADMIN:parola321@pprojects.ex2kh.mongodb.net/urls?retryWrites=true&w=majority',
+    baseUrl: 'http://localhost:5000'
 }
--- index.js
+++ index.js
@@ -3,12 +3,19 @@ const connectDB = require('./db')
 
 const app = express()
 
+//Database connection
+connectDB();
+
+//Routes
+app.use('/', require('./routes/index'))
+app.use('/api/url', require('./routes/url'))
 
 const PORT = process.env.PORT || 5000
+
 app.listen(PORT || 5000, () => {
     console.log(`Server running on port ${PORT}`)
 })
 
-connectDB();
+
 
 
