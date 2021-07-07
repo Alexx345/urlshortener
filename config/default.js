882f70210aaa70c64d34f3c8106f9c742f565c63
Alexx345 <65601648+Alexx345@users.noreply.github.com>
2021-07-07 19:38:04 +0300

    Update default.js

--- config/default.js
+++ config/default.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://ADMIN:parola321@pprojects.ex2kh.mongodb.net/urls?retryWrites=true&w=majority',
+    mongoURI: '',
     baseUrl: 'http://localhost:5000'
 }
 
