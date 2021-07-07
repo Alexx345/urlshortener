f45bb2b051704ffc1efc955805fae8e206982e6b
Alex <tanc.alexandru5@gmail.com>
2021-07-07 18:22:31 +0300

    second

---
+++ config/default.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://ADMIN:parola321@pprojects.ex2kh.mongodb.net/urls?retryWrites=true&w=majority'
+}
--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const connectDB = require('./db')
+
 const app = express()
 
-const PORT = process.env.PORT || 5000
 
+const PORT = process.env.PORT || 5000
 app.listen(PORT || 5000, () => {
     console.log(`Server running on port ${PORT}`)
 })
+
+connectDB();
+
+
